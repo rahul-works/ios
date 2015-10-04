ea971a037c5cff31633e9b3ffac99a82636f68c6
rahul thakur <rahul@acclivitysoftware.com>
2015-10-04 21:45:35 +0800

    Initial Commit

---
+++ MyFirstAppOnCousera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstAppOnCousera
+//
+//  Created by Rahul Thakur on 04/10/15.
+//  Copyright © 2015 Rahul Thakur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
